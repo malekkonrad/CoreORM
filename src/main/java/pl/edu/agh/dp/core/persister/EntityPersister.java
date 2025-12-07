66011ae3ae0c6b3c459b935b8f2f1dc6859cfbf0
Konrad Małek <konrad.malek1@gmail.com>
2025-12-07 16:27:00 +0100

    persister update

--- src/main/java/pl/edu/agh/dp/core/persister/EntityPersister.java
+++ src/main/java/pl/edu/agh/dp/core/persister/EntityPersister.java
@@ -1,4 +1,14 @@
 package pl.edu.agh.dp.core.persister;
 
+import pl.edu.agh.dp.api.Session;
+import pl.edu.agh.dp.core.mapping.metadata.EntityMetadata;
+
 public interface EntityPersister {
+    EntityMetadata metadata = null;
+    InheritanceStrategy inheritanceStrategy = null;
+
+    public Object findById(Object id, Session session);
+    public void insert(Object entity, Session session);
+    public void update(Object entity, Session session);
+    public void delete(Object entity, Session session);
 }
