29be1932b700346ae06bc908b0ae93bc2553cb1a
Konrad Małek <konrad.malek1@gmail.com>
2025-12-14 20:17:36 +0100

    update InheritanceMetadata and added InheritanceType

--- src/main/java/pl/edu/agh/dp/core/mapping/InheritanceMetadata.java
+++ src/main/java/pl/edu/agh/dp/core/mapping/InheritanceMetadata.java
@@ -1,10 +1,31 @@
 package pl.edu.agh.dp.core.mapping;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
 import java.util.Map;
 
+
+@Getter
+@Setter
 public class InheritanceMetadata {
 
-    boolean classTableInheritance;
-    String baseTable;
-    Map<Class<?>, String> subclassTables;
+    private final InheritanceType type;
+    private final String discriminatorColumnName; // tylko dla SINGLE_TABLE (opcjonalnie JOINED)
+    private final Map<Class<?>, String> classToDiscriminator;
+    private final Map<String, Class<?>> discriminatorToClass;
+    private final List<Class<?>> subclasses; // konkretne podklasy (bez root)
+
+    public InheritanceMetadata(InheritanceType type,
+                               String discriminatorColumnName,
+                               Map<Class<?>, String> classToDiscriminator,
+                               Map<String, Class<?>> discriminatorToClass,
+                               List<Class<?>> subclasses) {
+        this.type = type;
+        this.discriminatorColumnName = discriminatorColumnName;
+        this.classToDiscriminator = classToDiscriminator;
+        this.discriminatorToClass = discriminatorToClass;
+        this.subclasses = subclasses;
+    }
 }
---
+++ src/main/java/pl/edu/agh/dp/core/mapping/InheritanceType.java
@@ -0,0 +1,8 @@
+package pl.edu.agh.dp.core.mapping;
+
+public enum InheritanceType {
+    NONE,
+    SINGLE_TABLE,
+    JOINED,
+    TABLE_PER_CLASS
+}
