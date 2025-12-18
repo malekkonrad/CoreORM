95dc54af4ff69daa7fa5168030326ad15a3c847b
Konrad Małek <konrad.malek1@gmail.com>
2025-12-18 11:31:55 +0100

    inheritance strategy interface

--- src/main/java/pl/edu/agh/dp/core/persister/InheritanceStrategy.java
+++ src/main/java/pl/edu/agh/dp/core/persister/InheritanceStrategy.java
@@ -1,4 +1,13 @@
 package pl.edu.agh.dp.core.persister;
 
+import pl.edu.agh.dp.core.mapping.EntityMetadata;
+
+import java.util.List;
+
 public interface InheritanceStrategy {
+    Object insert(EntityMetadata rootMetadata, Object entity);
+    void update(EntityMetadata rootMetadata, Object entity);
+    void delete(EntityMetadata rootMetadata, Object entity);
+    <T> T findById(EntityMetadata rootMetadata, Class<T> type, Object id);
+    <T> List<T> findAll(EntityMetadata rootMetadata, Class<T> type);
 }
