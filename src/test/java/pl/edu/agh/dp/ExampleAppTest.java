7175774b5f63f06079840198002de2bb46fb474e
komumati <mateuszkotarba1@gmail.com>
2026-02-02 15:21:36 +0100

    exmaple app test

---
+++ src/test/java/pl/edu/agh/dp/ExampleAppTest.java
@@ -0,0 +1,70 @@
+package pl.edu.agh.dp;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pl.edu.agh.dp.api.Configuration;
+import pl.edu.agh.dp.api.Orm;
+import pl.edu.agh.dp.api.Session;
+import pl.edu.agh.dp.api.SessionFactory;
+import pl.edu.agh.dp.api.annotations.*;
+import pl.edu.agh.dp.core.mapping.InheritanceType;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+public class ExampleAppTest {
+
+
+    String url = System.getenv("DB_URL") != null
+            ? System.getenv("DB_URL")
+            : "jdbc:h2:./testdb_relationships;AUTO_SERVER=TRUE;MODE=PostgreSQL;DATABASE_TO_LOWER=TRUE;DEFAULT_NULL_ORDERING=HIGH";
+
+    String user = System.getenv("DB_USER") != null ? System.getenv("DB_USER") : "sa";
+    String password = System.getenv("DB_PASSWORD") != null ? System.getenv("DB_PASSWORD") : "";
+
+    Configuration config;
+    SessionFactory sessionFactory;
+    Session session;
+
+    Connection conn;
+    Statement stmt;
+
+    @BeforeEach
+    public void setUp() {
+        // Reset database
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            stmt = conn.createStatement();
+            stmt.execute("DROP ALL OBJECTS");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        config = Orm.configure()
+                .setProperty("db.url", url)
+                .setProperty("db.user", user)
+                .setProperty("db.password", password)
+                .setProperty("orm.schema.auto", "create");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (session != null) {
+            session.close();
+        }
+    }
+
+    @Test
+    public void appTest() {
+        config.register();
+        sessionFactory = config.buildSessionFactory();
+        session = sessionFactory.openSession();
+    }
+}
